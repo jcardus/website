9c6d678c5a485914052bb4412b176be9b74e0005
jcardus <asklocation.net@gmail.com>
2022-07-30 18:13:17 +0100

    lemavisima -> fleetmap

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,9 @@
         <br>Email: admin@fleetmap.io
         <br>Phone: +351967167715<br>
         <a href="https://fleetmap.freshdesk.com/">https://fleetmap.freshdesk.com/</a><br>
+        16192 Coastal Hwy
+        Lewes, Delaware DE 19958
+        USA
       </p>
     </b-jumbotron>
   </div>
