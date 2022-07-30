9aaf928b7a2c11a558aa8ef1c0c248f809b02205
jcardus <asklocation.net@gmail.com>
2022-07-30 18:11:58 +0100

    lemavisima -> fleetmap

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,23 @@
 <template>
-  <b-jumbotron>
-    <template #header>
-      Lemavisima Unipessoal
-    </template>
+  <div>
+    <b-jumbotron>
+      <template #header>
+        Fleetmap LLC
+      </template>
 
-    <template #lead>
-      We provide Fleetmap - a Tracking and fleet management platform to grow your business. Personalized and scalable oriented to loT.
-    </template>
+      <template #lead>
+        We provide Fleetmap - a Tracking and fleet management platform to grow your business.
+      </template>
 
-    <hr class="my-4">
-    <p>
-      You can reach us at the following channels:
-      <br>Email: suporte@fleetmap.io
-      <br>Phone: +44 7380 309948<br>
-      <a href="https://m.me/104462708215447">Messenger</a><br>
-      <a href="https://www.facebook.com/fleetmap.io">https://www.facebook.com/fleetmap.io</a><br>
-      <a href="https://fleetmap.freshdesk.com/">https://fleetmap.freshdesk.com/</a><br>
-    </p>
-  </b-jumbotron>
+      <hr class="my-4">
+      <p>
+        You can reach us at the following channels:
+        <br>Email: admin@fleetmap.io
+        <br>Phone: +351967167715<br>
+        <a href="https://fleetmap.freshdesk.com/">https://fleetmap.freshdesk.com/</a><br>
+      </p>
+    </b-jumbotron>
+  </div>
 </template>
 
 <script>
